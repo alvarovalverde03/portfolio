85c10fa0ad48b86784db267eeba164296ee5f4f9
Alvaro <65318965+alvarovalverde03@users.noreply.github.com>
2023-08-09 21:33:08 +0200

    refactor: :hammer: Refactor svg icons into components

---
+++ src/lib/icons/BackIcon.svelte
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" fill-rule="nonzero"  width="3rem" height="3rem">
+    <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+        <g transform="scale(10.12,10.12)">
+            <path d="M16.62 2.99c-.49-.49-1.28-.49-1.77 0L6.54 11.3c-.39.39-.39 1.02 0 1.41l8.31 8.31c.49.49 1.28.49 1.77 0s.49-1.28 0-1.77L9.38 12l7.25-7.25c.48-.48.48-1.28-.01-1.76z"></path>
+        </g>
+    </g>
+</svg>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { PageData } from './$types';
 	import { formatDate } from '$lib/utils';
+	import BackIcon from '$lib/icons/BackIcon.svelte';
 
     export let data: PageData;
 </script>
@@ -21,8 +22,7 @@
 </svelte:head>
 
 <a href="/" class="back">
-	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.12,10.12)"><path d="M16.62 2.99c-.49-.49-1.28-.49-1.77 0L6.54 11.3c-.39.39-.39 1.02 0 1.41l8.31 8.31c.49.49 1.28.49 1.77 0s.49-1.28 0-1.77L9.38 12l7.25-7.25c.48-.48.48-1.28-.01-1.76z"></path></g></g></svg>
-	
+	<BackIcon />
 </a>
 
 <article>
@@ -43,12 +43,6 @@
         display: block;
 		width: fit-content;
     }
-    .back svg {
-		width: 3rem;
-		height: 3rem;
-		
-		filter: var(--clr-svg);
-	}
 
 	article hgroup {
 		margin-top: var(--spacing-64);

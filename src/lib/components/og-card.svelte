e6c1e1eebd7ce6f3438f4a2f6f8b47bc5adfbf3d
Alvaro <65318965+alvarovalverde03@users.noreply.github.com>
2023-08-25 14:55:41 +0200

    feat: ✨ Add OG Image Generation (#3)

---
+++ src/lib/components/og-card.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let message = 'alvarovalverde.dev';
+</script>
+
+<div class="wrapper">
+    <span>{message}</span>
+</div>
+  
+<style>
+    .wrapper {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        letter-spacing: -.02em;
+        font-weight: 700;
+        background: #fff;
+        font-size: 40px;
+    }
+
+    span {
+        display: flex;
+		font-size: 50px;
+		background-color: #000;
+        color: #fff;
+		width: 80%;
+        padding: 1rem;
+        text-align: center;
+    }
+</style>
+
+
---
+++ src/routes/api/og/+server.ts
@@ -0,0 +1,47 @@
+import satori from 'satori'
+import { html } from 'satori-html'
+import { Resvg } from '@resvg/resvg-js'
+import OgCard from '$lib/components/og-card.svelte'
+
+const height = 400
+const width = 800
+
+const inter = await fetch('https://alvarovalverde.dev/fonts/Inter-Regular.ttf').then((res) =>
+    res.arrayBuffer()
+)
+
+/** @type {import('./$types').RequestHandler} */
+export const GET = async ({url}) => {
+    const message = url.searchParams.get('message') ?? undefined;
+    const result = OgCard.render({message});
+1
+    const element = html(`${result.html}<style>${result.css.code}</style>`);
+    
+
+    const svg = await satori(element, {
+        fonts: [
+            {
+                name: 'Inter',
+                data: inter,
+                style: 'normal'
+            }
+        ],
+        height, 
+        width
+    })
+
+    const resvg = new Resvg(svg, {
+        fitTo: {
+          mode: 'width',
+          value: width
+        }
+    })
+
+    const img = resvg.render();
+
+    return new Response(img.asPng(), {
+        headers: {
+          'content-type': 'image/png'
+        }
+    })
+}
